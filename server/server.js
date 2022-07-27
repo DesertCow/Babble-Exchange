0ba1ed2d731d61f72b035a362398073d61feea0c
Eric Nguyen <15enguyen4@gmail.com>
2022-07-27 14:41:29 -0700

    messages controllers and models set up but still needs tuning

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const socket = require("socket.io");
 const authRoutes = require("./routes/auth");
 const app = express();
-// const messageRoutes = require("./routes/messages");
+const messageRoutes = require("./routes/messages");
 require('dotenv').config();
 
 
@@ -25,9 +25,31 @@ mongoose.connect(process.env.MONGO_URL, {
 
 //* Setup API Routes
 app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 const server = app.listen(process.env.PORT, () => {
   // console.log(`Server Hosted on Port ${process.env.PORT}`)
   console.log(`| 🚀  Live API: \x1b[34mhttp://localhost:${process.env.PORT}/api\x1b[0m 🚀 |`);
 })
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
