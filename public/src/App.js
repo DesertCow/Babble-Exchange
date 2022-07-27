6cfcb12bead7af253a58a28ae96b1cd57b8131ed
donghohnation <donghoh.han@gmail.com>
2022-07-27 11:41:06 -0700

    transferred html to react format

--- public/src/App.js
+++ public/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Chat from './pages/Chat';
 import Login from './pages/Login';
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Chat/>} />
-      </Routes>
-    </BrowserRouter>
-  )
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/register" element={<Register />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/" element={<Chat/>} />
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+function App() {
+	return (
+		<div>
+			<Login />
+		</div>
+	);
 }
+
+export default App;
