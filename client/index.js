d957d0ab0338005b1f4b9bae4e6cf83f73ffe808
donghohnation <donghoh.han@gmail.com>
2022-07-26 22:12:35 -0700

    updated welcome, sign up, and chat windows

--- client/index.js
+++ client/index.js
@@ -1,7 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 // import App from "./components/App";
 // import "./styles.css";
 // import "./reset.css";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
+
+function openCity(evt, cityName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
