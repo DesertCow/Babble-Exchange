e3200b78452ae91483fb82a9b3ab3ecf37e08b11
Clayton S <desertcow07@gmail.com>
2022-07-27 13:45:16 -0700

    Enabled login page but still debug

---
+++ public/src/utils/apiRoutes.js
@@ -0,0 +1,7 @@
+export const host = "http://localhost:3001";
+export const loginRoute = `${host}/api/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
+export const logoutRoute = `${host}/api/auth/logout`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
