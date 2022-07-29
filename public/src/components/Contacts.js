e5a9dcc93943e091e12bb0bacd6d56b92feb1746
Clayton S <desertcow07@gmail.com>
2022-07-28 20:20:51 -0700

    Debug of Chat page WIP

---
+++ public/src/components/Contacts.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+export default function Contacts({ contacts, changeChat }) {
+
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentSelected, setCurrentSelected] = useState(undefined);
+
+  useEffect(async () => {
+    const data = await JSON.parse(
+      localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+    );
+    setCurrentUserName(data.username);
+  }, []);
+
+  const changeCurrentChat = (index, contact) => {
+    setCurrentSelected(index);
+    changeChat(contact);
+  };
+
+
+  return (
+    <>
+      <Container>
+        <div className="contacts">
+          {contacts.map((contact, index) => {
+            return (
+              <div
+                key={contact._id}
+                className={`contact ${index === currentSelected ? "selected" : ""
+                  }`}
+                onClick={() => changeCurrentChat(index, contact)}>
+                <div className="username">
+                  <h3>{contact.username}</h3>
+                </div>
+              </div>
+              // <div className="current-user">
+              //   <div className="username">
+              //     <h2>{currentUserName}</h2>
+              //   </div>
+              // </div>
+            );
+          })}
+        </div>
+      </Container>
+    </>
+  );
+}
+
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 10% 75% 15%;
+  overflow: hidden;
+  background-color: #080420;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 2rem;
+    }
+    h3 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  .contacts {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+    gap: 0.8rem;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #ffffff39;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    }
+    .contact {
+      background-color: #ffffff34;
+      min-height: 5rem;
+      cursor: pointer;
+      width: 90%;
+      border-radius: 0.2rem;
+      padding: 0.4rem;
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      .avatar {
+        img {
+          height: 3rem;
+        }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+      }
+    }
+    .selected {
+      background-color: #9a86f3;
+    }
+  }
+`;
--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -7,7 +7,7 @@ import styled from "styled-components";
 import { allUsersRoute, host } from "../utils/apiRoutes";
 import ChatContainer from "../components/ChatContainer";
 
-// import Contacts from "../components/Contacts";
+import Contacts from "../components/Contacts";
 // import Welcome from "../components/Welcome";
 
 
@@ -16,7 +16,7 @@ export default function Chat() {
 	const navigate = useNavigate();
 	const socket = useRef();
 
-	// const [contacts, setContacts] = useState([]);
+	const [contacts, setContacts] = useState([]);
 	const [currentChat, setCurrentChat] = useState(undefined);
 	const [currentUser, setCurrentUser] = useState(undefined);
 
@@ -45,7 +45,7 @@ export default function Chat() {
 				<div className="container">
 					<div changeChat={handleChatChange}>
 						{currentChat === undefined ? (
-							`No Current Chat AHHH CHAT:${currentChat}`
+							`No Current Chat -> CHAT:${currentChat}`
 						) : (
 							<ChatContainer currentChat={currentChat} socket={socket} />
 						)}
@@ -77,148 +77,3 @@ const Container = styled.div`
     }
   }
 `;
-
-
-
-
-
-// function Chat2() {
-
-// 	function openCity(evt, cityName) {
-// 		var i, tabcontent, tablinks;
-// 		tabcontent = document.getElementsByClassName("tabcontent");
-// 		for (i = 0; i < tabcontent.length; i++) {
-// 			tabcontent[i].style.display = "none";
-// 		}
-// 		tablinks = document.getElementsByClassName("tablinks");
-// 		for (i = 0; i < tablinks.length; i++) {
-// 			tablinks[i].className = tablinks[i].className.replace(" active", "");
-// 		}
-// 		document.getElementById(cityName).style.display = "block";
-// 		evt.currentTarget.className += " active";
-// 	}
-
-// 	// Get the element with id="defaultOpen" and click on it
-// 	document.getElementById("defaultOpen").click();
-
-// 	return (
-// 		<div className="row d-flex align-items-center justify-content-center">
-
-// 			<div className="chat col-7">
-// 				<div className="row">
-
-// 					<div className="tab col-3">
-// 						<button className="tablinks" onclick="openCity(event, 'Friend1')" id="defaultOpen">Friend1</button>
-// 						<button className="tablinks" onclick="openCity(event, 'Friend2')">Friend2</button>
-// 						<button className="tablinks" onclick="openCity(event, 'Friend3')">Friend3</button>
-// 						<button className="tablinks" onclick="openCity(event, 'Friend4')">Friend4</button>
-// 						<button className="tablinks" onclick="openCity(event, 'Friend5')">Friend5</button>
-// 						<button className="tablinks" onclick="openCity(event, 'Friend6')">Friend6</button>
-// 						<button className="tablinks" onclick="openCity(event, 'Friend7')">Friend7</button>
-// 						<button className="tablinks" onclick="openCity(event, 'Friend8')">Friend8</button>
-// 					</div>
-
-
-// 					<div className="contt col">
-
-
-// 						<div id="Friend1" className="tabcontent">
-// 							<h3 className="chatheader">Friend1 Chat</h3>
-// 							<div className="messages">
-// 								<p>Lorem Ipsum dolor</p>
-// 							</div>
-// 							<div className="text-center">
-// 								<input className="chatinput" type="text" />
-// 								<button className="sendbtn">send</button>
-// 							</div>
-// 						</div>
-
-// 						<div id="Friend2" className="tabcontent">
-// 							<h3 className="chatheader">Friend2 Chat</h3>
-// 							<div className="messages">
-// 								<p>Lorem Ipsum dolor</p>
-// 							</div>
-// 							<div className="text-center">
-// 								<input className="chatinput" type="text" />
-// 								<button className="sendbtn">send</button>
-// 							</div>
-// 						</div>
-
-// 						<div id="Friend3" className="tabcontent">
-// 							<h3 className="chatheader">Friend3 Chat</h3>
-// 							<div className="messages">
-// 								<p>Lorem Ipsum dolor</p>
-// 							</div>
-// 							<div className="text-center">
-// 								<input className="chatinput" type="text" />
-// 								<button className="sendbtn">send</button>
-// 							</div>
-// 						</div>
-
-// 						<div id="Friend4" className="tabcontent">
-// 							<h3 className="chatheader">Friend4 Chat</h3>
-// 							<div className="messages">
-// 								<p>Lorem Ipsum dolor</p>
-// 							</div>
-// 							<div className="text-center">
-// 								<input className="chatinput" type="text" />
-// 								<button className="sendbtn">send</button>
-// 							</div>
-// 						</div>
-
-// 						<div id="Friend5" className="tabcontent">
-// 							<h3 className="chatheader">Friend5 Chat</h3>
-// 							<div className="messages">
-// 								<p>Lorem Ipsum dolor</p>
-// 							</div>
-// 							<div className="text-center">
-// 								<input className="chatinput" type="text" />
-// 								<button className="sendbtn">send</button>
-// 							</div>
-// 						</div>
-
-// 						<div id="Friend6" className="tabcontent">
-// 							<h3 className="chatheader">Friend6 Chat</h3>
-// 							<div className="messages">
-// 								<p>Lorem Ipsum dolor</p>
-// 							</div>
-// 							<div className="text-center">
-// 								<input className="chatinput" type="text" />
-// 								<button className="sendbtn">send</button>
-// 							</div>
-// 						</div>
-
-// 						<div id="Friend7" className="tabcontent">
-// 							<h3 className="chatheader">Friend7 Chat</h3>
-// 							<div className="messages">
-// 								<p>Lorem Ipsum dolor</p>
-// 							</div>
-// 							<div className="text-center">
-// 								<input className="chatinput" type="text" />
-// 								<button className="sendbtn">send</button>
-// 							</div>
-// 						</div>
-
-// 						<div id="Friend8" className="tabcontent">
-// 							<h3 className="chatheader">Friend8 Chat</h3>
-// 							<div className="messages">
-// 								<p>Lorem Ipsum dolor</p>
-// 							</div>
-// 							<div className="text-center">
-// 								<input className="chatinput" type="text" />
-// 								<button className="sendbtn">send</button>
-// 							</div>
-// 						</div>
-
-
-// 					</div>
-
-
-
-// 				</div>
-// 			</div>
-// 		</div>
-// 	)
-// }
-
-// export default Chat;
